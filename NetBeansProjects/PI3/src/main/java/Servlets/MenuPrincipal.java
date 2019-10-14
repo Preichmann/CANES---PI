9588cd3a09972f7e01e2335847ac2dcf4a009f89
Gabriel Vital gabrelvital49@gmail.com <>
2019-10-14 16:50:23 -0300

    Configurado que o arquivo de abertura serja o Menu principal

---
+++ NetBeansProjects/PI3/src/main/java/Servlets/MenuPrincipal.java
@@ -0,0 +1,40 @@
+package Servlets;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author nik_r
+ */
+@WebServlet(name = "MenuPrincipal", urlPatterns = {"/menu-principal"})
+public class MenuPrincipal extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        RequestDispatcher dispatcher
+                = request.getRequestDispatcher("/WEB-INF/menu-principal.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        RequestDispatcher dispatcher
+                = request.getRequestDispatcher("/WEB-INF/menu-principal.jsp");
+        dispatcher.forward(request, response);
+    }
+}
